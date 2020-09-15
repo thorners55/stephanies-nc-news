907af754e63b3b8703ec7255bbdc41f50f2fe5bf
Stephanie Thornley <sthornley00@gmail.com>
2020-09-15 17:06:06 +0100

    Fix sort buttons callback function/state setting

--- src/components/SortButtons.jsx
+++ src/components/SortButtons.jsx
@@ -2,25 +2,30 @@ import React, { Component } from "react";
 
 class SortButtons extends Component {
   state = {
-    sort: "",
+    sort: "created_at",
     order: "desc",
   };
 
   handleClick = (event) => {
     event.preventDefault();
     const { name, value } = event.target;
+    console.log(name, value, "handleClick");
     this.setState((currentState) => {
       return { ...currentState, [name]: value };
     });
-    this.handleSubmit(event);
   };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { getArticles, topic, username } = this.props;
-    const { sort, order } = this.state;
-    getArticles(topic, username, sort, order);
-  };
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.sort !== prevState.sort ||
+      this.state.order !== prevState.order
+    ) {
+      const { getArticles, topic, username } = this.props;
+      console.log(this.state.sort, this.state.order);
+      const { sort, order } = this.state;
+      getArticles(topic, username, sort, order);
+    }
+  }
 
   render() {
     return (
